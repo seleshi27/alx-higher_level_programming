b95d3dc71b4213c679d6508bc726bfaaacd57b97
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2023-03-10 00:05:24 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-def arguments():
-    arg_string = "arguments"
-    c_or_d = "."
-    length = len(argv) - 1
-    if (length == 1):
-        arg_string = "argument"
-    if (length - 1 >= 0):
-        c_or_d = ":"
-
-    print("{} {}{}".format(length, arg_string, c_or_d,))
-
-    for index, arg in enumerate(argv):
-        if (index > 0):
-            print("{}: {}".format(index, arg))
-
 if __name__ == "__main__":
-    arguments()
+    from sys import argv
+    count = len(argv[1:])
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+    for i in range(1, (count + 1)):
+        print("{}: {}".format(i, argv[i]))
