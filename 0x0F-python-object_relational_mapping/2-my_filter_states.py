ee64484b8f22308f90ac0c9e4da57bae9041b32d
seleshi27 <110991479+seleshi27@users.noreply.github.com>
2023-06-18 23:14:37 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""A script that lists all states in a database."""
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    username, password, name, search = sys.argv[1:]
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=username,
-        passwd=password, db=name
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC;"
-        .format(search)
-    )
-    for state in cur.fetchall():
-        print(state)
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
